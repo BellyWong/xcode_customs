6230748ef4461be1886f25d99691f014364dcc48
Petroianu Daniel <petroianudaniel@gmail.com>
2013-07-24 21:55:01 +0300

    Added qualitycoding.org code snippets and templates

---
+++ Templates/iOS OCUnit Test Case/___FILEBASENAME___.m
@@ -0,0 +1,32 @@
+//
+//  ___PROJECTNAME___ - ___FILENAME___
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//  Created by: ___FULLUSERNAME___
+//
+
+
+#import "<#Class Header#>"
+
+
+#define HC_SHORTHAND
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <OCHamcrest/OCHamcrest.h>
+#import <OCMock/OCMock.h>
+
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+{
+    // test fixture ivars go here
+}
+
+- (void)test<#Method should return Expected result#>
+{
+
+}
+
+@end
